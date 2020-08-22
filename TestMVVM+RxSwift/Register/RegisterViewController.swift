9113a1aa1a5e76521a456fd778c6ea3a8e594c90
songpeng <308823810@qq.com>
2020-08-22 16:02:52 +0800

    update

--- TestMVVM+RxSwift/Register/RegisterViewController.swift
+++ TestMVVM+RxSwift/Register/RegisterViewController.swift
@@ -46,10 +46,10 @@ class RegisterViewController: UIViewController {
             wireframe: DefaultWireframe.shared)
 
         viewModel = RegisterViewModel(input: input, dependency: dependency)
-//        viewModel?.output.signupEnable.subscribe(onNext: { [weak self] (valid) in
-//            self?.registeredButton.isEnabled = valid
-//            self?.registeredButton.alpha = valid ? 1.0 : 0.5
-//        }).disposed(by: bag)
+        viewModel?.output.signupEnable.subscribe(onNext: { [weak self] (valid) in
+            self?.registeredButton.isEnabled = valid
+            self?.registeredButton.alpha = valid ? 1.0 : 0.5
+        }).disposed(by: bag)
 
         viewModel?.output.usernameValid.bind(to: userTipLabel.rx.validationResult).disposed(by: bag)
         viewModel?.output.passwordValid.bind(to: passwordTipLabel.rx.validationResult).disposed(by: bag)
@@ -59,30 +59,30 @@ class RegisterViewController: UIViewController {
         }).disposed(by: bag)
         
         
-        let second = GithubService.sharedValidationService.API.usernameAvailable("123456song123")
-        let registerObservable = Observable.combineLatest(registeredButton.rx.tap, second).flatMap { (_,isValidate) -> Observable<Result<String,Error>> in
-            if isValidate{
-                print("用户名有效")
-                return GithubService.sharedValidationService.API.signup("123", password: "123456").map { (isSuccess) -> Result<String,Error> in
-                    return isSuccess ? Result.success("注册成功") : Result.failure(SampleError())
-                }.catchError { (error) -> Observable<Result<String, Error>> in
-                    Observable.just(Result.failure(error))
-                }
-            }else{
-                print("用户名无效")
-                return Observable.just(Result<String,Error>.failure(SampleError()))
-            }
-        }
-        registerObservable.subscribe(onNext: { (result) in
-            switch result{
-            case .success(let string):
-                print("注册成功，没有异常-\(string)")
-            case .failure(let error):
-                print("注册失败，出现异常了 = \(error)")
-            }
-        }, onError: { (error) in
-            
-        }).disposed(by: bag)    
+//        let second = GithubService.sharedValidationService.API.usernameAvailable("123456song123")
+//        let registerObservable = Observable.combineLatest(registeredButton.rx.tap, second).flatMap { (_,isValidate) -> Observable<Result<String,Error>> in
+//            if isValidate{
+//                print("用户名有效")
+//                return GithubService.sharedValidationService.API.signup("123", password: "123456").map { (isSuccess) -> Result<String,Error> in
+//                    return isSuccess ? Result.success("注册成功") : Result.failure(SampleError())
+//                }.catchError { (error) -> Observable<Result<String, Error>> in
+//                    Observable.just(Result.failure(error))
+//                }
+//            }else{
+//                print("用户名无效")
+//                return Observable.just(Result<String,Error>.failure(SampleError()))
+//            }
+//        }
+//        registerObservable.subscribe(onNext: { (result) in
+//            switch result{
+//            case .success(let string):
+//                print("注册成功，没有异常-\(string)")
+//            case .failure(let error):
+//                print("注册失败，出现异常了 = \(error)")
+//            }
+//        }, onError: { (error) in
+//
+//        }).disposed(by: bag)
     }
     
     
